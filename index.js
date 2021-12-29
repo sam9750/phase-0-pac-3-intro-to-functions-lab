1d3480fee13a9dd2bae90102511f9eb0f5ab821d
Sam <sam.wadhwa95@gmail.com>
2021-12-29 13:35:21 -0500

    IN PROGRESS

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string){
+return string.toUpperCase();
+}
+function whisper(string){
+    return string.toLowerCase();
+}
+
+function logShout(string){
+    console.log(string.toUpperCase());
+}
+
+function logWhisper(string){
+    console.log(string.toLowerCase());
+}
+
+
+function sayHiToHeadphonedRoomate(string){
+    console.log (string.toLowerCase());
+    if (string.toLowerCase() === string);
+     console.log("I can't hear you!"); }
+     if (string.toUpperCase() === string) {
+     console.log("YES INDEED!");
+     }
+     else if (string === "Let's have dinner together!");
+     console.log("I would love to!"); 
+
+     sayHiToHeadphonedRoomate()
